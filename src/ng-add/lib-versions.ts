c998637b1bc59c1d5eae262142307cf6e5a62545
Zack Yang <zack9433@gmail.com>
2018-08-29 03:08:49 +0800

    chore: upgrade package

--- src/ng-add/lib-versions.ts
+++ src/ng-add/lib-versions.ts
@@ -1,5 +1,5 @@
-export const angularVersion = '^6.1.1';
-export const ngxFormlyVersion = '^5.0.0-beta.2';
+export const angularVersion = '^6.1.4';
+export const ngxFormlyVersion = '^5.0.0-beta.3';
 export const ngxTranslateVersion = '^10.0.2';
-export const ngrxStoreVersion = '^6.0.1';
-export const tpUiVersion = '^0.0.8';
+export const ngrxStoreVersion = '^6.1.0';
+export const tpUiVersion = '^0.0.11';
